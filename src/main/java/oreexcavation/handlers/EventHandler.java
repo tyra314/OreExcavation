37d74ec57de993667236f1f86b016cb8397856d5
Funwayguy <FunwayguyEmail@gmail.com>
2016-09-14 22:45:24 +1000

    Added option to invert blacklist to whitelist Added ore dictionary support for tools & blocks

--- src/main/java/oreexcavation/handlers/EventHandler.java
+++ src/main/java/oreexcavation/handlers/EventHandler.java
@@ -7,7 +7,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.oredict.OreDictionary;
