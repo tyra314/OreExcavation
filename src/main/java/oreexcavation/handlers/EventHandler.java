8b59f11bc492ca88a8c7f3f12d46fd51014d2d52
Funwayguy <FunwayguyEmail@gmail.com>
2016-09-23 07:06:50 +1000

    Fixed key bind self cancelling

--- src/main/java/oreexcavation/handlers/EventHandler.java
+++ src/main/java/oreexcavation/handlers/EventHandler.java
@@ -111,7 +111,7 @@ public class EventHandler
 			canContinue = false;
 		} else if(ExcavationSettings.mineMode == 0)
 		{
-			if(!ExcavationKeys.veinKey.isPressed())
+			if(!ExcavationKeys.veinKey.getIsKeyPressed())
 			{
 				canContinue = false;
 			}
